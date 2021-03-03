737e78157143848282a1c577310f4e8ed870cf3d
Jessica Chapdelaine <jessicachap223@gmail.com>
2021-03-03 21:08:56 +0000

    Fixed bugs in power and multiplication

--- arithmetic.py
+++ arithmetic.py
@@ -15,7 +15,7 @@ def subtract(num1, num2):
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
-    return num * num2
+    return num1 * num2
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
@@ -34,7 +34,7 @@ def cube(num1):
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
-    num1**num2
+    return num1**num2
 
 
 def mod(num1, num2):
